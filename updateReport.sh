a35d37dccf80498f1bf1a4d9ef249bfc5febecdb
☣️ Arka ☣️ <arkamtg.pramanik@gmail.com>
2023-04-25 13:18:41 +0530

    Fix script to also delete service

--- updateReport.sh
+++ updateReport.sh
@@ -9,6 +9,7 @@ do
   microk8s kubectl logs $(microk8s kubectl get pods --all-namespaces -o jsonpath='{.items[?(@.metadata.labels.app=="kube-bench")].metadata.name}') | jq . > kube-bench.json
   docker build -t 952910/node-server .
   docker push 952910/node-server
+  microk8s kubectl delete service nodeapp-service nodeapp-service2
   microk8s kubectl delete deployment nodeapp-deployment
   microk8s kubectl apply -f node-deploy.yaml
   sleep 3600
