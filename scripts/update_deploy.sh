e0d9d133b704c4703718c3d74a6047f62d7dd986
☣ Arka ☣ <arkamtg.pramanik@gmail.com>
2023-11-23 22:02:24 +0530

    Update version of Trivy and Dashboard

--- scripts/update_deploy.sh
+++ scripts/update_deploy.sh
@@ -2,9 +2,10 @@ microk8s helm repo add prometheus-community https://prometheus-community.github.
 microk8s helm repo add aqua https://aquasecurity.github.io/helm-charts/
 microk8s helm repo update
 microk8s kubectl create ns monitoring
+microk8s kubectl delete ns trivy-system
 microk8s helm upgrade --install prom prometheus-community/kube-prometheus-stack -n monitoring --values ./values/prom-values.yaml
-microk8s helm upgrade --install trivy-operator aqua/trivy-operator \
+microk8s helm install trivy-operator aqua/trivy-operator \
   --namespace trivy-system \
   --create-namespace \
-  --version 0.18.4 \
+  --version 0.19.0-rc \
   --values ./values/trivy-values.yaml
